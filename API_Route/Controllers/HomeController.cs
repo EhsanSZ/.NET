74cc0cd17c5d007d62854e83c3b0e44443b140dc
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-09-10 23:52:21 +0430

    Route

---
+++ API_Route/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Route.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+         
+        [HttpGet]
+        [Route("~/Products", Order =-1)]
+        public IActionResult Product()
+        {
+            return Ok();
+        }
+
+
+        [HttpGet]
+        [Route("MyApi")]
+        [Route("MyApi/Index")]
+        [Route("MyApi/Index/{id?}")]
+        [Route("[controller]/[action]")]
+        [Route("api/[controller]/[action]")]
+        public IActionResult Index(int id)
+        {
+            return Ok(id);
+        }
+
+        [HttpGet]
+        [Route("book/list")]
+        [Route("~/book/list")]
+        public IActionResult BookList()
+        {
+            return Ok(true);
+        }
+    }
+}
---
+++ API_Route/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace API_Route.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("~/Products" , Name ="productList" , Order =-2 )]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
+
+
