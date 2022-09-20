f0e5363d1236796de1adaf57c1cacc18951fcd4e
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-09-20 12:47:17 +0430

    Open Redirect , CSRF

---
+++ Security/Controllers/Attacks.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Security.Controllers
+{
+    public class Attacks : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OpenRediret(bool succeeded , string url)
+        {
+            //if(succeeded)
+                //return LocalRedirect(url);
+
+            if (Url.IsLocalUrl(url))
+                return Redirect(url);
+            else
+            {
+                // Logg
+               return RedirectToAction(nameof(Index));
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CSRF(string name , string blog)
+        {
+            return View();
+        }
+    }
+}
