a700e8390b08dbcc224d9259b03f2e5489c12dc6
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-09-29 14:51:13 +0330

    Redis

---
+++ Redis/Models/Dto/ProductDto.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+
+namespace Redis.Models.Dto
+{
+    public class ProductDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    public class HomePageDto
+    {
+        public LastProduct LastProduct { get; set; }
+        public BestProduct  BestProduct { get; set; }
+    }
+
+    public class LastProduct
+    {
+        public List<ProductDto> Products { get; set; }
+    }
+
+    public class BestProduct
+    {
+        public List<ProductDto> Products { get; set; }
+
+    }
+}
---
+++ Redis/Models/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using Redis.Models.Dto;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Redis.Models
+{
+    public interface IProductRepository
+    {
+        HomePageDto GetHomePageProducts();
+    }
+    public class ProductRepository: IProductRepository
+    {
+        public HomePageDto GetHomePageProducts()
+        {
+            Thread.Sleep(3000);
+            return new HomePageDto()
+            {
+                 BestProduct = new BestProduct()
+                {
+                    Products = new List<ProductDto>()
+                      {
+                           new ProductDto { Id=1, Name="آموزش اصول  Solid"},
+                           new ProductDto { Id=2, Name="آموزش الگوهای طراحی"}
+                      }
+                },
+                 LastProduct = new LastProduct()
+                {
+                    Products = new List<ProductDto>()
+                      {
+                           new ProductDto { Id=3, Name="آموزش میکروسرویس"},
+                           new ProductDto { Id=4, Name="آموزش DDD"},
+                           new ProductDto { Id=5, Name="آموزش پیشرفته سی شارپ"}
+                      }
+                }
+            };
+        }
+    }
+}
