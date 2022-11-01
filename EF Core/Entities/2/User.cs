4e0d7fb2d7b0bbad344367eb5a635b17646ca2d8
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-11-01 22:04:36 +0330

    Query

---
+++ EF Core/Entities/2/User.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+ 
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsRemoved { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
