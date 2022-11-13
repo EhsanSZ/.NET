c7ea01adf94b8901007745fc873dceeabb243f3e
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-11-13 21:48:35 +0330

    Linq1

---
+++ Linq/Filter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq.Filter
+{
+    public class Filter
+    {
+        public static void Filter_Sample()
+        {
+
+            var result = GetCourselist().Where((c, i) =>
+             {
+
+                 if (i % 2 == 0)
+                     return true;
+
+                 return false;
+             }
+            );
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            IList list = new ArrayList();
+            list.Add(100);
+            list.Add("Bugeto.net");
+            list.Add("babaei");
+            list.Add("nader");
+            list.Add(5);
+            list.Add(new Course { Id = 1, Name = "test ", Price = 0 });
+
+            var intResult = list.OfType<int>();
+            var strResult = list.OfType<string>();
+
+        }
+
+        private static List<Course> GetCourselist()
+        {
+            List<Course> courses = new List<Course>()
+            {
+                 new Course { Id=1, Name="c-sharp" , Price= 10000},
+                 new Course { Id=2, Name="Asp net core" , Price= 20000},
+                 new Course { Id=3, Name="html" , Price= 5000},
+                 new Course { Id=4, Name="css" , Price= 7000},
+                 new Course { Id=5, Name="MongoDb" , Price= 55000},
+                 new Course { Id = 6, Name = "Android", Price = 18000 }
+            };
+
+            return courses;
+        }
+    }
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
+
---
+++ Linq/Sort.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Sort
+{
+    public class Sort
+    {
+        public static void Sort_Example()
+        {
+
+            var dataOrderBy = GetCourselist().OrderBy(p => p.Price);
+
+            Console.WriteLine("__________________OrderBy_______________________");
+
+            foreach (var item in dataOrderBy)
+            {
+                Console.WriteLine($"Id = {item.Id}      Name = {item.Name}   Price = {item.Price}");
+            }
+
+
+
+
+            var ThenBy = GetCourselist().OrderBy(p => p.Price).ThenBy(p => p.Name);
+
+            Console.WriteLine("__________________ThenBy_______________________");
+
+            foreach (var item in ThenBy)
+            {
+                Console.WriteLine($"Id = {item.Id}      Name = {item.Name}   Price = {item.Price}");
+            }
+
+
+
+
+
+            var Descending = GetCourselist().OrderByDescending(p => p.Price);
+
+
+            Console.WriteLine("__________________Descending_______________________");
+
+            foreach (var item in Descending)
+            {
+                Console.WriteLine($"Id = {item.Id}      Name = {item.Name}   Price = {item.Price}");
+            }
+
+
+
+            var ThenByDescending = GetCourselist().OrderByDescending(p => p.Price).ThenByDescending(p => p.Id);
+
+
+            Console.WriteLine("__________________ThenByDescending_______________________");
+
+            foreach (var item in ThenByDescending)
+            {
+                Console.WriteLine($"Id = {item.Id}      Name = {item.Name}   Price = {item.Price}");
+            }
+
+
+
+
+            Console.WriteLine("__________________Reverse_______________________");
+
+            foreach (var item in ThenByDescending.Reverse())
+            {
+                Console.WriteLine($"Id = {item.Id}      Name = {item.Name}   Price = {item.Price}");
+            }
+
+
+        }
+
+
+        private static List<Course> GetCourselist()
+        {
+            List<Course> courses = new List<Course>()
+            {
+                 new Course { Id=1000, Name="Asp net core" , Price= 3500},
+                 new Course { Id=1001, Name="MongoDb" , Price= 7100},
+                 new Course { Id =1002, Name = "Android", Price = 3500 },
+            };
+
+            return courses;
+        }
+    }
+
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
