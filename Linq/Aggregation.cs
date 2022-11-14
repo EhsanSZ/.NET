0bd46b51c89159fac4b4ef22a66e373c256ed8f8
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-11-14 19:54:25 +0330

    Linq2

---
+++ Linq/Aggregation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Aggregation
+{
+    public class Aggregation
+    {
+        public static void Aggregation_Method()
+        {
+
+            List<string> data = new List<string>() { "Asp.net Core", "Csharp", "Php", "Java", "nodeJs", "Go" };
+
+            var sep = data.Aggregate((s1, s2) => s1 + "____,=+___" + s2);
+            Console.WriteLine(sep);
+
+
+            Console.WriteLine("______________________________________");
+
+
+
+            List<Product> products = new List<Product>()
+            {
+                new Product { Name = "Asp.Net Core", Price = 10000 },
+                 new Product { Name = "C-Shart", Price = 15000 },
+                 new Product { Name = "C++", Price = 5000 },
+                 new Product { Name = "PHP", Price = 25000 },
+                 new Product { Name = "Java", Price = 8000 },
+            };
+
+            string sep2 = products.Aggregate<Product, string, string>("Products Name :"
+                , (str, p) => str += p.Name + ","
+                , str => str.Substring(0, str.Length - 1));
+
+            Console.WriteLine(sep2);
+            Console.WriteLine("____________________Average__________________");
+
+            Console.WriteLine(products.Average(p => p.Price).ToString());
+            Console.WriteLine("____________________Count__________________");
+
+            Console.WriteLine(products.LongCount().ToString());
+
+            Console.WriteLine("____________________Max__________________");
+
+            Console.WriteLine(products.Max(p => p.Price).ToString());
+
+            Console.WriteLine("____________________Min__________________");
+
+            Console.WriteLine(products.Min(p => p.Price).ToString());
+
+            Console.WriteLine("____________________Sum__________________");
+
+            Console.WriteLine(products.Sum(p => p.Price).ToString());
+
+        }
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Linq/Elements.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Elements
+{
+    public class Elements
+    {
+        static void Elements_Method(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product { Name = "Asp.Net Core", Price = 10000 },
+                 new Product { Name = "C-Sharp", Price = 15000 },
+                 new Product { Name = "C++", Price = 5000 },
+                 new Product { Name = "PHP", Price = 25000 },
+                 new Product { Name = "Java", Price = 8000 },
+                 new Product { Name = "Java-h", Price = 9000 },
+            };
+
+            List<Product> productsEmp = new List<Product>();
+
+            var product = products.ElementAt(0);
+            var product2 = productsEmp.ElementAtOrDefault(0);
+
+            var productFirst = products.First(p => p.Name.Contains("h"));
+            var productFirstempty = productsEmp.FirstOrDefault();
+
+            var productLast = products.Last(p => p.Name.Contains("h"));
+            var productLastempty = productsEmp.LastOrDefault();
+
+
+            List<Product> productsSinle = new List<Product>();
+            //{
+            //    new Product { Name = "Asp.Net Core Single", Price = 10000 },
+
+            //};
+
+
+            var single = productsSinle.SingleOrDefault();
+
+            Console.ReadLine();
+        }
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
