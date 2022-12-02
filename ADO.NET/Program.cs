f21068979ded80cc23833ecf6585cd8305ba1fd6
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-12-02 23:22:12 +0330

    ADO.NET V1

---
+++ ADO.NET/Program.cs
@@ -0,0 +1,18 @@
+﻿using AdoSamples.ConsoleUI;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System;
+
+namespace ADO.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample1 _samples = new Sample1();
+            _samples.SqlBulkCopySample();
+
+            Console.ReadKey();
+        }
+    }
+}
