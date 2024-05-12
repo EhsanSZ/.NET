4f75eb4b618b1c98e5ff4c7af53a58aea3f10591
EhsanSZ <e.seyedzadeh73@gmail.com>
2024-05-12 22:22:08 +0330

    ef

---
+++ EF Core/06. EfAdvancedConfigSample/EfAdvancedConfigSample/Program.cs
@@ -0,0 +1,6 @@
+﻿using EfAdvancedConfigSample.TemporalTables;
+
+PersonTemporalContext Context = new PersonTemporalContext();
+PersonTemporalRepository repository = new(Context);
+repository.PrintAllWithHistory();
+Console.ReadLine();
