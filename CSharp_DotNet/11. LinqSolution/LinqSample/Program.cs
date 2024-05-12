0fce96a2c672881bb0fe29463f749cea0fe2ea5c
EhsanSZ <e.seyedzadeh73@gmail.com>
2024-05-12 22:02:40 +0330

    c sharp

---
+++ CSharp_DotNet/11. LinqSolution/LinqSample/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LinqSample;
+
+LinqOperators linqOperators = new LinqOperators();
+linqOperators.GroupJoin();
+linqOperators.PrintNamesWithLinqMethod();
